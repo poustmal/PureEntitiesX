d13b2af17b69ee34229b3ac441ae0daabe6ddf4e
Joshua <joshua67.r@gmail.com>
2017-01-30 17:51:09 -0500

    Update Rabbit.php

--- src/revivalpmmp/pureentities/entity/animal/jumping/Rabbit.php
+++ src/revivalpmmp/pureentities/entity/animal/jumping/Rabbit.php
@@ -1,1 +1,40 @@
+<?php
 
+namespace revivalpmmp\pureentities\entity\animal\jumping;
+
+use revivalpmmp\pureentities\entity\animal\WalkingAnimal;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\entity\Creature;
+use revivalpmmp\pureentities\data\Data;
+
+class Rabbit extends WalkingAnimal{
+  
+    const NETWORK_ID = Data::RABBIT;
+    public $width = 0.5;
+    public $height = 0.5;
+    public function getSpeed() : float{
+        return 1.2;
+    }
+    
+    public function getName(){
+        return "Rabbit";
+    }
+  
+    public function initEntity(){
+        parent::initEntity();
+        $this->setMaxHealth(3);
+        $this->setHealth(3);
+    }
+  
+    public function targetOption(Creature $creature, float $distance) : bool{
+        if($creature instanceof Player){
+            return $creature->spawned && $creature->isAlive() && !$creature->closed && $creature->getInventory()->getItemInHand()->getId() == Item::SEEDS && $distance <= 49;
+        }
+        return false;
+    }
+  
+    public function getDrops(){
+        return [];
+    }
+}
